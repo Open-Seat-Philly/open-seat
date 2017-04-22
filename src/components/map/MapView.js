c5d2ce0363d0bb84a319061e9c99c17be6b49cd3
cccots <ecotsare@oberlin.edu>
2017-04-22 01:04:45 -0400

    Almost displays open seats on map

--- src/components/map/MapView.js
+++ src/components/map/MapView.js
@@ -3,6 +3,7 @@ import Mapbox, { Layer, Feature } from 'react-mapbox-gl';
 import DivisionPopup from './DivisionPopup';
 import { openSeatsPropType } from './propTypes';
 import divisionData from '../../data/divisions.json';
+import openSeatsData from '../../data/OpenSeats.json';
 
 const ZOOM = 10;
 const CENTER = [-75.1452, 39.9826];
@@ -45,6 +46,7 @@ export default class MapView extends Component {
     const { openSeats } = this.props;
     const { selectedDivision, selectedDivisionSeats } = this.state;
 
+
     return (
       <div className='map-view'>
         <Mapbox
@@ -53,15 +55,53 @@ export default class MapView extends Component {
           style='mapbox://styles/mapbox/dark-v9'
           accessToken={process.env.MAPBOX_TOKEN}
         >
-          <Layer type='fill'>
-            {divisionData.features.map(division => (
-              <Feature
-                key={division.properties['OBJECTID']}
-                onClick={this.handleDivisionClick}
-                coordinates={division.geometry.coordinates}
-                properties={division.properties}
-              />
-            ))}
+
+        //divisions layer outlines every division
+        <Layer
+          id='divisions'
+          type='line'
+          sourceId='divisions'
+          layerOptions={{
+            source: {
+              type: 'geojson',
+              data: divisionData
+            }
+          }}
+          paint={{'line-width':.5}}
+        />
+
+        //seats layer colors in districts with uncontested seats
+        <Layer
+          id='seats'
+          type='fill'
+          paint={{
+            "fill-color":"rgba(32, 149, 252, .9)",
+            "fill-opacity":.25
+          }}
+          >
+          //make an array of features as children to the seats layer
+          {divisionData.features
+            .map(division => {
+              let wardNum = division.properties['DIVISION_NUM'].slice(0,2);
+              let divisionNum = division.properties['DIVISION_NUM'].slice(2);
+              //search for an open seat
+              for(let seat of openSeatsData){
+                //if we find one for this division add a feature
+                if ((seat['WARD'] === wardNum) && (seat['DIVISION'] === divisionNum)){
+
+                  return (
+                    <Feature
+                      key={division.properties['OBJECTID']}
+                      onClick={this.handleDivisionClick}
+                      coordinates={division.geometry.coordinates}
+                      properties={division.properties}
+                    />
+                  );
+                }
+              }
+              return(null); //no seat found so we return null (which means the feature array has null values)
+            })
+            .filter(division => (null !== division))} //remove null values from feature array
           </Layer>
 
           {selectedDivision && (
@@ -74,5 +114,7 @@ export default class MapView extends Component {
         </Mapbox>
       </div>
     );
+
+
   }
 }
